c08030a2171a753fcafb6168948c0585e167eff6
huxudong <158633490@qq.com>
2017-03-01 09:28:23 +0800

    update

---
+++ src/main/java/com/act/bbs/action/BaseController.java
@@ -0,0 +1,31 @@
+package com.act.bbs.action;
+
+import act.app.ActionContext;
+import act.controller.Controller;
+import act.db.ebean.EbeanDao;
+import com.act.bbs.model.BbsModule;
+import org.osgl.mvc.annotation.Before;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Created by huxudong on 17/2/27.
+ */
+public class BaseController extends Controller.Util {
+
+
+    @Inject
+    private EbeanDao<Integer, BbsModule> moduleDao;
+
+    private static String PAGENAME;
+
+
+
+    @Before
+    public void initRenderArgs(ActionContext context) {
+        context.renderArg("pagename",PAGENAME);
+        List<BbsModule> moduleList = moduleDao.findAllAsList();
+        context.renderArg("moduleList",moduleList);
+    }
+}
---
+++ src/main/java/com/act/bbs/common/Const.java
@@ -0,0 +1,21 @@
+package com.act.bbs.common;
+
+/**
+ * Created by huxudong on 17/2/9.
+ */
+public class Const{
+
+
+    public static final int BBS_TOPIC_SCORE = 10;
+    public static final int BBS_POST_SCORE = 3;
+    public static final int BBS_REPLAY_SCORE = 3;
+
+
+    //level 用户等级
+
+    public static final int REFRESH_THRESHOLD = 30;
+    public static final int OLD_THRESHOLD = 100;
+    public static final int TEACHER_THRESHOLD = 200;
+    public static final int DIRECTOR_THRESHOLD = 350;
+    public static final int PRESIDENT_THRESHOLD = 700;
+}
